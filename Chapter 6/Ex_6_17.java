332a4d9a45bccfa8480d56e78e20f522d97566b8
AustinH1050 <42474755+AustinH1050@users.noreply.github.com>
2018-12-14 09:47:34 -0700

    Create Ex_6_17.java

---
+++ Chapter 6/Ex_6_17.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Ex_6_17 {
+	
+	public static void main(String[] args) {
+		//Scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Variables
+		int n = 0;
+		
+		//Prompt
+		System.out.print("Enter n: ");
+		n = input.nextInt();
+
+		//Matrix
+		printMatrix(n);
+	}
+
+	
+	public static void printMatrix(int n) {
+		for (int row = 0; row < n; row++) {
+			for (int colum = 0; colum < n; colum ++) {
+				
+				//Display a random 0 or 1
+				System.out.print((int)(Math.random() * 2) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
