7835cf5c1554aa0de1f2b6d9eef6138660f1e66b
AustinH1050 <42474755+AustinH1050@users.noreply.github.com>
2019-03-04 09:35:54 -0700

    Create Ex_8_37.java

---
+++ Chapter 8/Ex_8_37.java
@@ -0,0 +1,19 @@
+/*
+Author: Austin Haueter
+Date: 2/26/2019
+*/
+
+import java.util.Scanner;
+
+class Ex_8_37 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double state [][] = new double [10][2];
+		double other [][] = new double [10][2];
+		double [][] output = new double [10][2];
+		
+		for (int row = 0; row< 10; row++) {
+			System.out.print("Enter 10 States: " + row + 1);
+		}
+		}
+}
